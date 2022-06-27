0a7deae3c0aac5f5bfb0e307363b8ba07df732a4
juanpa77 <juanpa.box.77@gmail.com>
2022-06-27 14:39:45 -0300

    add method for upgrade document of db Pages and getCategories

--- src/db.js
+++ src/db.js
@@ -1,13 +1,45 @@
 
 import { getFirestore } from "firebase/firestore/lite";
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, doc, updateDoc, arrayUnion, setDoc, query, where, getDoc } from "firebase/firestore";
 import app from "./credentials";
-
-const db = getFirestore(app)
 
 export const addToBookmarks = async (page) => {
   const addPage = await addDoc(collection(db, 'bookmarks'), {
-    path: page
+    path: page.url,
+    title: page.title
   })
 }
+
+class Db {
+  constructor() {
+    this.db = getFirestore(app)
+  }
+
+  async getCategories() {
+    const categoryRef = collection(db, 'category')
+    const test = await getDoc(categoryRef)
+    return test
+  }
+
+  async addToBookmarks(page, category) {
+    const pageRef = doc(this.db, 'bookmarks', category)
+    updateDoc(pageRef, {
+      pages: arrayUnion({
+        url: page.url,
+        title: page.title
+      })
+    },
+    {
+      merge: true
+    })
+  }
+
+  adCategory() {
+    
+  }
+
+}
+
+export default Db
+
 // console.log("Document written with ID: ", docRef.id)
